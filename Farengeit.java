698ac7d92e8e526ec72238f9cf22f980ebce5bc4
helenmartiian <helenmartiian@gmail.com>
2016-06-13 19:40:41 +0300

    Add files via upload
    Add Farengeit.java

---
+++ Farengeit.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * Created by Елена on 27/05/2016.
+ */
+public class Farengeit {
+
+    //static int a;
+
+    public static void main(String[] args) {
+      // int cels;
+        System.out.println(getFar(40));
+    }
+
+        public static float getFar(int cels)
+        {
+
+
+            return (((float)9/5)*cels + 32);
+
+
+        }
+
+    }
+
+
